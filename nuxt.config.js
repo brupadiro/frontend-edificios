a009adeac9a21de08faca841bba0aeda3b49af76
brupadiro <brunodiharce@gmail.com>
2022-10-26 10:20:49 -0300

    url de backlog

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,7 +61,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://edificios-oa6js.ondigitalocean.app/api',
+    baseURL: 'https://backlog.gestus360.com/api',
     //baseURL:'http://localhost:1337/api',
   },
 
